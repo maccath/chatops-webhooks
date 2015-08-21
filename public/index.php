5785750f97e08ef2e1489749bb1723ab2d0d604d
Katy Ereira <katy.ereira@castletrust.co.uk>
2015-08-21 15:17:09 +0100

    Basic Slim application skeleton.

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
-<?php phpinfo(); ?>
+<?php
+
+// Setup autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Prepare app
+$settings = require __DIR__ . '/../app/settings.php';
+$app = new \Slim\App($settings);
+
+// Register dependencies
+require __DIR__ . '/../app/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../app/middleware.php';
+
+// Register routes
+require __DIR__ . '/../app/routes.php';
+
+// Run app
+$app->run();
