27d2f4ebfa630823800f4acba602918249a16ab6
Katy Ereira <katy.ereira@castletrust.co.uk>
2016-03-03 14:38:50 +0000

    Add basic authenticator test

---
+++ app/tests/Authenticators/BasicTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Tests\Authenticators;
+
+/**
+ * Class SlackTest
+ */
+class BasicTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that authentication fails when request token doesn't match authenticator token
+     *
+     * @expectedException \Exception
+     */
+    public function testCheckAuthNoMatchWithToken()
+    {
+        $request = $this->getMockBuilder('\Slim\HTTP\Request')->disableOriginalConstructor()->getMock();
+        $request->method('getParam')->willReturn('badtoken');
+
+        $authenticator = new \App\Authenticators\Basic();
+        $authenticator->applySettings(['token' => 'validtoken']);
+
+        $authenticator->check($request);
+    }
+
+    /**
+     * Test that authentication passes when request token matches authenticator token
+     */
+    public function testCheckAuthMatchWithToken()
+    {
+        $request = $this->getMockBuilder('\Slim\HTTP\Request')->disableOriginalConstructor()->getMock();
+        $request->method('getParam')->willReturn('validtoken');
+
+        $authenticator = new \App\Authenticators\Basic();
+        $authenticator->applySettings(['token' => 'validtoken']);
+
+        $authenticator->check($request);
+    }
+
+    /**
+     * Test that authentication passes when authenticator token is false regardless of request token value
+     */
+    public function testCheckAuthMatchWithoutToken()
+    {
+        $request = $this->getMockBuilder('\Slim\HTTP\Request')->disableOriginalConstructor()->getMock();
+        $request->method('getParam')->willReturn('validtoken');
+
+        $authenticator = new \App\Authenticators\Basic();
+        $authenticator->applySettings([]);
+
+        $authenticator->check($request);
+    }
+}
